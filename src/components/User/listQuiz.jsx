467bce977b64b9b7e2df6fdf1cecbc2862928b41
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2023-12-20 20:18:28 +0700

    cm

--- src/components/User/listQuiz.jsx
+++ src/components/User/listQuiz.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { getQuizByUser } from '../../services/apiservice';
+import './listQuiz.scss'
 const ListQuiz = (props) => {
     const[listQuiz, setlistQuiz] = useState([]);
     const fetchQuizData = async () => {
@@ -8,7 +9,7 @@ const ListQuiz = (props) => {
         if (result && result.EC===0) {
             // console.log(result)
             setlistQuiz(result.DT);
-            console.log(listQuiz)
+            // console.log(listQuiz)
         }
     }
     useEffect(() => {
@@ -17,15 +18,15 @@ const ListQuiz = (props) => {
     return (
     
        <>
-       <div className='listQuiz-container container-fluid d-flex flex-row gap-5'>
+       <div className='listQuiz-container container-fluid d-flex flex-row gap-5 p-5'>
            {listQuiz && listQuiz.map((item, index) => {
            return (
-               <div key={`${index}-Quiz`} className="card" style={{ width: '18rem' }}>
+               <div key={`${index}-Quiz`} className="card" style={{ width: '18rem',borderRadius:'5px' }}>
                    <img src={`data:image/png;base64,${item.image}`} className="card-img-top" alt="..." />
                    <div className="card-body">
-                       <h5 className="card-title">Card title</h5>
-                       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                       <a href="#" className="btn btn-primary">Go somewhere</a>
+                       <h5 className="card-title">Quiz {index+1}</h5>
+                       <p className="card-text">{item.description}</p>
+                       <a href="#" className="btn btn-primary">Doing Quizz!!</a>
                    </div>
                </div>
            )
