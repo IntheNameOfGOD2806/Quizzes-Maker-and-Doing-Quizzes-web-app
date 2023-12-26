5e9730cca6a9b26b6915c34cf67bde69c25f8e3c
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2023-12-26 11:48:29 +0700

    commit mid day 26/12

--- src/components/Admin/Content/ManageQuestion.jsx
+++ src/components/Admin/Content/ManageQuestion.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./ManageQuestion.scss";
 import { FcPlus } from "react-icons/fc";
 import { FaTrash } from "react-icons/fa";
+import { PiPlusSquareThin } from "react-icons/pi";
+import { HiOutlineMinusCircle } from "react-icons/hi2";
 const ManageQuestion = (props) => {
   const options = [
     { value: "chocolate", label: "Chocolate" },
@@ -22,28 +24,127 @@ const ManageQuestion = (props) => {
           className=""
         />
       </div>
-       <div style={{ marginTop: "30px" }}><span>Add question:</span></div>
-      <div  class="form-floating mb-3 col-6 ">
-        <input
-          type="email"
-          class="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-        />
-        <label style={{color:"#ccc"}} for="floatingInput">Description</label>
+      <div style={{ marginTop: "30px" }}>
+        <span>Add question:</span>
       </div>
-      <div style={{flexDirection:"row"}} className=" d-flex gap-3 ">
-        <label htmlFor="">Upload Image:</label>
-        <input type="file" name="" id=""  />
+      <div className="form-add-new-wrapper col-6">
+        <div class="form-floating mb-3 col-6 ">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label style={{ color: "#ccc" }} for="floatingInput">
+            Description
+          </label>
+        </div>
+        <div className="icon-wrapper">
+          <div className="question-image-upload">
+            {/* <label htmlFor="">Upload Image:</label> */}
+            <input type="file" name="" id="" className="form-control" />
+          </div>
+          <span
+            className="icon-container"
+            style={{ display: "flex", gap: "10px", marginLeft: "20px" }}
+          >
+            <span>
+              <FcPlus
+                className="icon"
+                style={{ height: "30px", width: "30px" }}
+              />
+            </span>
+            <span>
+              <FaTrash
+                className="icon"
+                style={{ height: "30px", width: "30px", color: "red" }}
+              />
+            </span>
+          </span>
+        </div>
       </div>
-      <div>
-        <span>
-          <FcPlus className="icon" style={{ height: "30px", width: "30px" }} />
-        </span>
-        <span>
-          <FaTrash className="icon" style={{ height: "30px", width: "30px" }} />
-        </span>
+      {/* answer container */}
+      <div className="answer-container">
+        <div className="answer-add-new-wrapper ">
+          <input
+            style={{ marginRight: "10px" }}
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <div class="form-floating mb-3 col-4 ">
+            <input
+              type="text"
+              class="form-control "
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label style={{ color: "#ccc" }} for="floatingInput">
+              Answer 1
+            </label>
+          </div>
+          <span
+            className="icon-container"
+            style={{ display: "flex", gap: "10px", marginLeft: "20px" }}
+          >
+            <span>
+              <PiPlusSquareThin
+                className="icon"
+                style={{ height: "30px", width: "30px", color: "green" }}
+              />
+            </span>
+            <span>
+              <HiOutlineMinusCircle
+                className="icon"
+                style={{ height: "30px", width: "30px", color: "red" }}
+              />
+            </span>
+          </span>
+        </div>
       </div>
+      {/* end of answer container */}
+         {/* answer container */}
+         <div className="answer-container">
+        <div className="answer-add-new-wrapper ">
+          <input
+            style={{ marginRight: "10px" }}
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <div class="form-floating mb-3 col-4 ">
+            <input
+              type="text"
+              class="form-control "
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label style={{ color: "#ccc" }} for="floatingInput">
+              Answer 1
+            </label>
+          </div>
+          <span
+            className="icon-container"
+            style={{ display: "flex", gap: "10px", marginLeft: "20px" }}
+          >
+            <span>
+              <PiPlusSquareThin
+                className="icon"
+                style={{ height: "30px", width: "30px", color: "green" }}
+              />
+            </span>
+            <span>
+              <HiOutlineMinusCircle
+                className="icon"
+                style={{ height: "30px", width: "30px", color: "red" }}
+              />
+            </span>
+          </span>
+        </div>
+      </div>
+      {/* end of answer container */}
     </div>
   );
 };
